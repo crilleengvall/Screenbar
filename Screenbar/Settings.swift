a780e13819b02d643e6d14cd188bc0ef6afa2d00
Christian Engvall <crilleengvall@gmail.com>
2016-05-10 21:07:52 +0200

    Creating key for seconds

--- Screenbar/Settings.swift
+++ Screenbar/Settings.swift
@@ -2,14 +2,16 @@ import Foundation
 
 class Settings : NSObject {
 
+    static var secondsKey = "seconds"
+    
     static func setSecondsIntervall(seconds: Double?) {
         let defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setDouble(seconds!, forKey: "seconds")
+        defaults.setDouble(seconds!, forKey: secondsKey)
     }
     
     static func getSecondsInterval() -> Double? {
         let defaults = NSUserDefaults.standardUserDefaults()
-        var seconds: Double? = defaults.doubleForKey("seconds")
+        var seconds: Double? = defaults.doubleForKey(secondsKey)
         if(seconds == nil) {
             seconds = 1.0
         }
