8ac8b7cb58c2e1d2e148a6b5d1f756e6c50ae383
Christian Engvall <crilleengvall@gmail.com>
2016-06-25 12:47:28 +0200

    Updating to latest NSharpDate

--- Screenbar/NSharpDate.swift
+++ Screenbar/NSharpDate.swift
@@ -2,10 +2,39 @@ import Foundation
 
 public extension NSDate {
     
-    func stringByShortTimeFormat() -> String {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = NSDateFormatterStyle.NoStyle;
-        dateFormatter.timeStyle = NSDateFormatterStyle.MediumStyle;
-        return dateFormatter.stringFromDate(self)
+    private var dateFormatter: NSDateFormatter? {
+        get {
+            var formatter = objc_getAssociatedObject(self, "NSharpDateSwiftFormatter") as? NSDateFormatter
+            if(formatter == nil) {
+                formatter = NSDateFormatter()
+                objc_setAssociatedObject(self, "NSharpDateSwiftFormatter", formatter as NSDateFormatter?, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+            }
+            return formatter;
+        }
+    }
+    
+    var day : Int {
+        self.dateFormatter?.dateFormat = "dd"
+        let day = self.dateFormatter?.stringFromDate(self)
+        return Int(day!)!
+    }
+    
+    var dayOfWeek : String {
+        self.dateFormatter?.dateFormat = "EEEE"
+        return (self.dateFormatter?.stringFromDate(self))!
+    }
+    
+    var dayOfYear : Int {
+        let calendar = NSCalendar.currentCalendar()
+        let dayOfYear = calendar.ordinalityOfUnit(NSCalendarUnit.Day, inUnit: NSCalendarUnit.Year, forDate: self)
+        return dayOfYear
+    }
+    
+    // MARK: - Methods
+    
+    func toStringByShortTimeFormat() -> String {
+        self.dateFormatter!.dateStyle = NSDateFormatterStyle.NoStyle
+        self.dateFormatter!.timeStyle = NSDateFormatterStyle.MediumStyle
+        return self.dateFormatter!.stringFromDate(self)
     }
 }
--- Screenbar/ScreenShot.swift
+++ Screenbar/ScreenShot.swift
@@ -4,7 +4,7 @@ class ScreenShot : NSObject {
     
     func take() {
         let date = NSDate()
-        let dateString = date.stringByShortTimeFormat().stringByReplacingOccurrencesOfString(":", withString: ".", options: NSStringCompareOptions.LiteralSearch, range: nil)        
+        let dateString = date.toStringByShortTimeFormat().stringByReplacingOccurrencesOfString(":", withString: ".", options: NSStringCompareOptions.LiteralSearch, range: nil)
         let task = NSTask()
         task.launchPath = "/usr/sbin/screencapture"
         task.arguments = [NSHomeDirectory() + "/Desktop/Screenshot-" + dateString + ".png"]
